f44830a24633c61b48abb5c8053f228c97ec1df3
jx-dohwan <79912056+jx-dohwan@users.noreply.github.com>
2023-02-02 22:14:29 +0900

    Add files via upload

--- lightning-text-classification/classifier.py
+++ lightning-text-classification/classifier.py
@@ -1,6 +1,6 @@
 # https://github.com/ricardorei/lightning-text-classification/blob/master/classifier.py
 import logging as log
-from argparse import ArguemtParser, Namespace
+from argparse import ArgumentParser, Namespace
 from collections import OrderedDict
 
 import numpy as np
@@ -11,7 +11,7 @@ import torch.nn as nn
 from torch import optim
 from torch.utils.data import DataLoader, RandomSampler
 from torchnlp.encoders import LabelEncoder
-from torchnlp.utils import collate_tensors, 
+from torchnlp.utils import collate_tensors, lengths_to_mask
 from transformers import AutoModel
 
 from tokenizer import Tokenizer
@@ -142,6 +142,228 @@ class Classifier(pl.Lightningmodule):
                 for prediction in np.argmax(logits, axis=1)
             ]
             sample['predicted_label'] = predicted_labels[0]
-            
+
         return sample
 
+    def forward(self, tokens, lengths):
+        """Usual pytorch forward function.
+        :param tokens: text sequences [batch_size x src_seq_len]
+        :param lengths: source lengths [batch_size]
+        Returns:
+            Dictionary with model outputs(e.g:logits)
+        """
+        tokens = tokens[:,:lengths.max()]
+        # When using just one GPU this should not change behavior
+        # but when splitting batches across GPU the tokens have padding
+        # from the entire origianl batch
+        mask = lengths_to_mask(lengths, device=tokens.device)
+
+        # RUN BERT model
+        word_embeddings = self.bert(tokens, mask)[0]
+
+        # Average Poling
+        word_embeddings = mask_fill(
+            0.0, tokens, word_embeddings, self.tokenizer.padding_index
+        )
+        sentemb = torch.sum(word_embeddings, 1)
+        sum_mask = mask.unsqueeze(-1).expand(word_embeddings.size()).float().sum(1)
+        sentemb = sentemb / sum_mask
+
+        return {'logits':self.classification_head(sentemb)}
+
+    def loss(self, predictions:dict, targets:dict) -> torch.tensor:
+        """
+        Computes Loss value according to a loss function.
+        :param predictions: model specific output. Must contain a key 'logits' with
+            a tensor [batch_size x 1] with model predictions
+        :param labels: Label values[batch_size]
+
+        Returns:
+            torch.tensor with loss value.
+        """
+        return self._loss(predictions['logits'], targets['labels'])
+
+    def prepare_sample(self, sample:list, prepare_target: bool=True):
+        """
+        Function that prepares a sample to input the model.
+        :param sample : list of dictionaries.
+        Returns:
+            - dictionary with the expected model inputs.
+            - dictionary with the expected target labels.
+        """
+        sample = collate_tensors(sample)
+        tokens, lengths = self.tokenizer.batch_encode(sample['text'])
+        inputs = {'tokens':tokens, 'lengths':lengths}
+
+        if not prepare_target:
+            return inputs, {}
+
+        # Prepare target:
+        try:
+            targets = {'labels':self.data.label_encoder.batch_encode(sample['label'])}
+            return inputs, targets
+        except RuntimeError:
+            raise Exception("Label encoder found an unknown label")
+
+    def training_step(self, batch: tuple, batch_nb: int, *args, **kwargs) -> dict:
+        """
+        Runs one training step. This usually consists in the forward function followed
+            by the loss function.
+        :param batch: The output of your dataloader.
+        :param batch_nb: Integer displaying which batch this is
+        Returns:
+            - dictionary containing the loss and the metrics to be added to the lightning logger.
+        """
+        inputs, targets = batch
+        model_out = self.forward(**inputs)
+        loss_val = self.loss(model_out, targets)
+
+        # in DP mode (default) make sure if result is scalar, there's another dim in the beginning
+        if self.trainer.use_dp or self.trainer.use_ddp2:
+            loss_val = loss_val.unsqueeze(0)
+
+        output = OrderedDict({"loss": loss_val})
+        # can also return just a scalar instead of a dict (return loss_val)
+        return output
+
+    def validation_step(self, batch:tuple, batch_nb:int, *args, **kwargs) -> dict:
+        """Similar to the training step but with the model in eval mode.
+        Returns:
+            - dictionary passed to the validation_end function.
+        """
+        inputs, targets = batch
+        model_out = self.forward(**inputs)
+        loss_val = self.loss(model_out, targets)
+
+        y = targets['labels']
+        y_hat = model_out['logits']
+
+        # acc
+        labels_hat = torch.argmax(y_hat, dim=1)
+        val_acc = torch.sum(y == labels_hat).item() / (len(y)*1.0)
+        val_acc = torch.tensor(val_acc)
+
+        if self.on_gpu:
+            val_acc = val_acc.cuda(loss_val.device.index)
+
+        # in DP mode (default) make sure if result is scalar, there's another dim in the beginning
+        if self.trainer.use_dp or self.trainer.use_ddp2:
+            loss_val = loss_val.unsqueeze(0)
+            val_acc = val_acc.unsqueeze(0)
+
+        output = OrderedDict(
+            {
+                'val_loss':loss_val,
+                'val_acc':val_acc,
+            }
+        )
+
+        return output
+
+    def validation_end(self, outputs:list) -> dict:
+        """Function that takes as input a list of dictionaries returned by the validation_step
+        function and measures the model performance accross the entire validation set.
+        Returns:
+            - Dictionary with metrics to be added to the lightning logger.
+        """
+        val_loss_mean = 0
+        val_acc_mean = 0
+        for output in outputs:
+            val_loss = output['val_loss']
+
+            # reduce manually when using dp
+            if self.trainer.use_dp or self.trainer.use_ddp2:
+                val_loss = torch.mean(val_loss)
+            val_loss_mean += val_loss
+
+            # reduce manually when using dp
+            val_acc = output['val_acc']
+            if self.trainer.use_dp or self.trainer.use_ddp2:
+                val_acc = torch.mean(val_acc)
+            
+            val_acc_maen += val_acc
+        
+        val_loss_mean /= len(outputs)
+        val_acc_mean /= len(outputs)
+        tqdm_dict = {'val_loss':val_loss_mean, 'val_acc':val_acc_mean}
+        result = {
+            'progress_bar':tqdm_dict,
+            'log':tqdm_dict,
+            'val_loss':val_loss_mean,
+        }
+        return result
+
+    def configure_optimizers(self):
+        """sets different Lanrning rates for different parameter groups."""
+        parameters = [
+            {'params':self.classification_head.parameters()},
+            {
+                'params':self.bert.parameters(),
+                'lr':self.hparams.encoder_learning_rate,
+            },
+        ]
+        optimizer= optim.Adam(parameters, lr=self.hparams.learning_rate)
+        return [optimizer], []
+
+    def on_epoch_end(self):
+        """Pytorch lighting hook"""
+        if self.current_epoch + 1 >= self.nr_frozen_epochs:
+            self.unfreeze_encoder()
+
+    @classmethod
+    def add_model_specific_args(cls, parser: ArgumentParser) -> ArgumentParser:
+        """Parser for Estimator specific arguments/hyperparameters.
+        :param parser: argparse.ArgumentParser
+        Returns:
+            - updated parser
+        """
+        parser.add_argument(
+            "--encoder_model",
+            default="bert-base-uncased",
+            type=str,
+            help="Encoder model to be used.",
+        )
+        parser.add_argument(
+            "--encoder_learning_rate",
+            default=1e-05,
+            type=float,
+            help="Encoder specific learning rate.",
+        )
+        parser.add_argument(
+            "--learning_rate",
+            default=3e-05,
+            type=float,
+            help="Classification head learning rate.",
+        )
+        parser.add_argument(
+            "--nr_frozen_epochs",
+            default=1,
+            type=int,
+            help="Number of epochs we want to keep the encoder model frozen.",
+        )
+        parser.add_argument(
+            "--train_csv",
+            default="data/imdb_reviews_train.csv",
+            type=str,
+            help="Path to the file containing the train data.",
+        )
+        parser.add_argument(
+            "--dev_csv",
+            default="data/imdb_reviews_test.csv",
+            type=str,
+            help="Path to the file containing the dev data.",
+        )
+        parser.add_argument(
+            "--test_csv",
+            default="data/imdb_reviews_test.csv",
+            type=str,
+            help="Path to the file containing the dev data.",
+        )
+        parser.add_argument(
+            "--loader_workers",
+            default=8,
+            type=int,
+            help="How many subprocesses to use for data loading. 0 means that \
+                the data will be loaded in the main process.",
+        )
+        return 
